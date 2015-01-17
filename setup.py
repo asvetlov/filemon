f19057ca3bc87651c5e2b6f265bb7e7ee320b3a2
Andrew Svetlov <andrew.svetlov@gmail.com>
2015-01-17 21:27:40 +0200

    Initial implementation

---
+++ setup.py
@@ -0,0 +1,27 @@
+import codecs
+import os
+import re
+from setuptools import setup, find_packages
+
+
+with codecs.open(os.path.join(os.path.abspath(os.path.dirname(
+        __file__)), 'filemon', '__init__.py'), 'r', 'latin1') as fp:
+    try:
+        version = re.findall(r"^__version__ = '([^']+)'\r?$",
+                             fp.read(), re.M)[0]
+    except IndexError:
+        raise RuntimeError('Unable to determine version.')
+
+
+install_requires = ['PySide']
+
+
+setup(name='filemon',
+      version=version,
+      description="File Monitor",
+      author="Andrew Svetlov",
+      license="Apache 2",
+      packages=find_packages(),
+      install_requires=install_requires,
+      include_package_data=True,
+      entry_points={'console_scripts': ['filemon = filemon.main:main']})
